3111e7b5fbaf6149d21d3ce04a6b43e0af2d4859
Jan Nawrocki <jan.nawrocki06@gmail.com>
2024-09-08 23:23:49 +0200

    feat: implement ArticleService with ArticleRepository integration

--- service/service.go
+++ service/service.go
@@ -1,3 +1,62 @@
 package service
 
-type ArticleService struct{}
+import (
+	"context"
+	"errors"
+
+	"github.com/jannawro/blog/articles"
+)
+
+type ArticleService struct {
+	repo articles.ArticleRepository
+}
+
+func NewArticleService(repo articles.ArticleRepository) *ArticleService {
+	return &ArticleService{repo: repo}
+}
+
+func (s *ArticleService) Create(ctx context.Context, articleData []byte) (*articles.Article, error) {
+	var article articles.Article
+	if err := articles.UnmarshalToArticle(articleData, &article); err != nil {
+		return nil, err
+	}
+	return s.repo.Create(ctx, article)
+}
+
+func (s *ArticleService) GetAll(ctx context.Context) (articles.Articles, error) {
+	return s.repo.GetAll(ctx)
+}
+
+func (s *ArticleService) GetByTitle(ctx context.Context, title string) (*articles.Article, error) {
+	return s.repo.GetByTitle(ctx, title)
+}
+
+func (s *ArticleService) GetByTags(ctx context.Context, tags []string) (articles.Articles, error) {
+	return s.repo.GetByTags(ctx, tags)
+}
+
+func (s *ArticleService) UpdateByTitle(ctx context.Context, title string, updatedData []byte) (*articles.Article, error) {
+	existingArticle, err := s.repo.GetByTitle(ctx, title)
+	if err != nil {
+		return nil, err
+	}
+
+	var updatedArticle articles.Article
+	if err := articles.UnmarshalToArticle(updatedData, &updatedArticle); err != nil {
+		return nil, err
+	}
+
+	updatedArticle.ID = existingArticle.ID
+	return s.repo.Update(ctx, existingArticle.ID, updatedArticle)
+}
+
+func (s *ArticleService) DeleteByTitle(ctx context.Context, title string) error {
+	article, err := s.repo.GetByTitle(ctx, title)
+	if err != nil {
+		return err
+	}
+	if article == nil {
+		return errors.New("article not found")
+	}
+	return s.repo.Delete(ctx, article.ID)
+}
