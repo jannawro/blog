850de2e0de6e7b72c68617c4bb290484842b4562
Jan Nawrocki <jan.nawrocki06@gmail.com>
2024-09-13 19:30:35 +0200

    feat: implement HTMLHandler for serving blog component

--- handlers/html/handlers.go
+++ handlers/html/handlers.go
@@ -1,1 +1,36 @@
 package html
+
+import (
+	"net/http"
+
+	"github.com/jannawro/blog/article"
+	"github.com/jannawro/blog/components"
+)
+
+type HTMLHandler struct {
+	service *article.Service
+}
+
+func NewHTMLHandler(service *article.Service) *HTMLHandler {
+	return &HTMLHandler{service: service}
+}
+
+func (h *HTMLHandler) ServeBlog() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		articles, err := h.service.GetAll(ctx, nil) // Assuming no sorting for now
+		if err != nil {
+			http.Error(w, "Failed to fetch articles", http.StatusInternalServerError)
+			return
+		}
+
+		blogComponent := components.Blog(articles)
+
+		err = blogComponent.Render(ctx, w)
+		if err != nil {
+			http.Error(w, "Failed to render blog", http.StatusInternalServerError)
+			return
+		}
+	}
+}
