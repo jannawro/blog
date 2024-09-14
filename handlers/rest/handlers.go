8e9dadcc5982e93947d42b724347cf34a6c6d475
Jan Nawrocki <jan.nawrocki06@gmail.com>
2024-09-14 23:26:30 +0200

    feat: add Handler struct to wrap Service in REST API

--- handlers/rest/handlers.go
+++ handlers/rest/handlers.go
@@ -1,1 +1,15 @@
 package rest
+
+import (
+	"github.com/your-username/your-repo-name/article"
+)
+
+type Handler struct {
+	service *article.Service
+}
+
+func NewHandler(service *article.Service) *Handler {
+	return &Handler{
+		service: service,
+	}
+}
