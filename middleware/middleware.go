7af646dadb5c22ad6fac1b7d144b35db22a7b7dc
Jan Nawrocki <jan.nawrocki06@gmail.com>
2024-09-21 20:36:16 +0200

    feat: implement in-memory cache middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -4,6 +4,8 @@ import "net/http"
 
 type Middleware func(http.Handler) http.Handler
 
+type CacheKeyFunc func(*http.Request) string
+
 func CreateStack(xs ...Middleware) Middleware {
 	return func(next http.Handler) http.Handler {
 		for i := len(xs) - 1; i >= 0; i-- {
